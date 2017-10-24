e0e64a09980228e502957e7bb28ccd34695de56a
it_akumi <it.akumi1011@gmail.com>
2017-10-24 23:38:40 +0900

    Add basic test cases only for HTTP status code

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+import falcon
+from falcon import testing
+import pytest
+
+from app.main import api
+
+
+@pytest.fixture
+def client():
+    return testing.TestClient(api)
+
+
+def test_get_root(client):
+    response = client.simulate_get('/')
+    assert response.status == falcon.HTTP_NO_CONTENT
+
+def test_get_spots(client):
+    response = client.simulate_get('/spots')
+    assert response.status == falcon.HTTP_OK
+
+def test_post_spots(client):
+    response = client.simulate_post('/spots')
+    assert response.status == falcon.HTTP_CREATED
