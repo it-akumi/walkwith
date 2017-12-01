deb8141d70ba2b828ba4e71b6afd12c3b1e6e8f2
it_akumi <it.akumi1011@gmail.com>
2017-12-01 19:42:48 +0900

    Add AllSpots and SingleSpot class

--- app/main.py
+++ app/main.py
@@ -2,19 +2,24 @@
 import falcon
 
 from app.root import Root
-from app.spots import Spot
+from app.spots import AllSpots
+from app.spots import SingleSpot
 
 
 def create_api():
+    """Bind resources to URL."""
     api = falcon.API()
 
     root = Root()
-    spots = Spot()
+    all_spots = AllSpots()
+    single_spot = SingleSpot()
     api.add_route('/', root)
-    api.add_route('/spots', spots)
+    api.add_route('/spots', all_spots)
+    api.add_route('/spots/{spot_id}', single_spot)
 
     return api
 
 
 def main():
+    """Run application."""
     return create_api()
--- app/spots.py
+++ app/spots.py
@@ -10,17 +10,19 @@ from app.db import init_db
 
 class Spot():
     def __init__(self):
-        """Create session (interface for DB)."""
+        """Set session and attributes of spot."""
         Session = sessionmaker(bind=init_db())
-        self.session = Session()
+        self._session = Session()
+        self._attr = ['name', 'latitude', 'longitude', 'guide']
 
+
+class AllSpots(Spot):
     def on_get(self, req, resp):
-        """Return all spots in the form of json."""
-        all_spots = self.session.query(Spots).all()
-        attributes = ['name', 'latitude', 'longitude', 'guide']
+        """Return attributes of all spots in the form of json."""
+        all_spots = self._session.query(Spots).all()
         body = dict()
         body['spots'] = list(
-            {attr: spot.__dict__[attr] for attr in attributes}
+            {attr: spot.__dict__[attr] for attr in self._attr}
             for spot in all_spots
         )
 
@@ -31,3 +33,18 @@ class Spot():
     def on_post(self, req, resp):
         """Create new spot and return its location."""
         resp.status = falcon.HTTP_CREATED
+
+
+class SingleSpot(Spot):
+    def on_get(self, req, resp, spot_id):
+        """Return attributes of spot in the form of json."""
+        spot = self._session.query(Spots).get(spot_id)
+        if spot is None:
+            resp.body = 'Sorry, Resource Not Found.'
+            resp.status = falcon.HTTP_NOT_FOUND
+        else:
+            body = {attr: spot.__dict__[attr]
+                for attr in self._attr}
+            resp.body = json.dumps(body)
+            resp.content_type = falcon.MEDIA_JSON
+            resp.status = falcon.HTTP_OK
