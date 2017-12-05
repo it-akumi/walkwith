c258bd1195f74c39d6c61853e448f26473c181b6
it_akumi <it.akumi1011@gmail.com>
2017-12-05 09:04:38 +0900

    Add fixtures (db and db_table) to test

--- tests/test_spots.py
+++ tests/test_spots.py
@@ -6,7 +6,31 @@ import falcon
 from falcon import testing
 import pytest
 
+from app.db import Base
+from app.db import DB
 from app.main import create_api
+
+
+@pytest.fixture(scope='session')
+def db():
+    os.environ['PG_USER'] = os.getenv('PG_TEST_USER', '')
+    os.environ['PG_PASSWORD'] = os.getenv('PG_TEST_PASSWORD', '')
+    os.environ['PG_HOST'] = os.getenv('PG_TEST_HOST', '')
+    os.environ['PG_PORT'] = os.getenv('PG_TEST_PORT', '')
+    os.environ['PG_DBNAME'] = os.getenv('PG_TEST_DBNAME', '')
+
+    db = DB()
+    return db
+
+
+@pytest.fixture(scope='session')
+def db_table(request, db):
+    Base.metadata.create_all(bind=db.engine)
+
+    def teardown():
+        Base.metadata.drop_all(bind=db.engine)
+
+    # request.addfinalizer(teardown)
 
 
 @pytest.fixture
@@ -14,21 +38,11 @@ def client():
     return testing.TestClient(create_api())
 
 
-def setup_module(module):
-    os.environ['PG_USER'] = os.getenv('PG_TEST_USER', '')
-    os.environ['PG_PASSWORD'] = os.getenv('PG_TEST_PASSWORD', '')
-    os.environ['PG_HOST'] = os.getenv('PG_TEST_HOST', '')
-    os.environ['PG_PORT'] = os.getenv('PG_TEST_PORT', '')
-    os.environ['PG_DBNAME'] = os.getenv('PG_TEST_DBNAME', '')
-
-
-def teardown_module(module):
-    pass
+pytestmark = pytest.mark.usefixtures('db_table')
 
 
 def test_get_all_spots(client):
     all_spots = {'spots': []}
-
     response = client.simulate_get('/spots')
     assert response.headers['content-type'] == falcon.MEDIA_JSON
     assert json.loads(response.content) == all_spots
@@ -43,7 +57,6 @@ def test_post_spot(client):
         "guide": "Try to create new spot."
     }'''
     response = client.simulate_post('/spots', body=params)
-
     assert response.headers['location'] == '/spots/1'
     assert response.status == falcon.HTTP_CREATED
 
@@ -56,7 +69,6 @@ def test_get_non_existing_spot(client):
 def test_get_existing_spot(client):
     response = client.simulate_get('/spots/1')
     spot = b'{"name": "test spot", "latitude": 35.658581, "longitude": 139.745433, "guide": "Try to create new spot."}'
-
     assert response.headers['content-type'] == falcon.MEDIA_JSON
     assert response.content == spot
     assert response.status == falcon.HTTP_OK
