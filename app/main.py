01e9d2940873c0893a358f464a49725d9b70ef08
it_akumi <it.akumi1011@gmail.com>
2017-12-06 12:08:30 +0900

    Move initialization of DB to main

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
 # coding:utf-8
 import falcon
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from app.db import DB
+from app.db import Base, database
 from app.root import Root
-from app.spots import AllSpots
-from app.spots import SingleSpot
+from app.spots import AllSpots, SingleSpot
 
 
-def create_api():
+def create_api(session):
     """Bind resources to URL."""
     api = falcon.API()
-    db = DB()
 
     root = Root()
-    all_spots = AllSpots(db.session)
-    single_spot = SingleSpot(db.session)
+    all_spots = AllSpots(session)
+    single_spot = SingleSpot(session)
     api.add_route('/', root)
     api.add_route('/spots', all_spots)
     api.add_route('/spots/{spot_id}', single_spot)
@@ -24,4 +24,7 @@ def create_api():
 
 def main():
     """Run application."""
-    return create_api()
+    engine = create_engine(database())
+    Base.metadata.create_all(bind=engine)
+    session = sessionmaker(bind=engine)()
+    return create_api(session)
