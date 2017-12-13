19dbb34ab314c1a740fd2ef558ddd9653dc0b611
it_akumi <it.akumi1011@gmail.com>
2017-12-13 10:18:24 +0900

    Change response for 'GET /' from 200 to 301

--- app/root.py
+++ app/root.py
@@ -4,8 +4,5 @@ import falcon
 
 class Root():
     def on_get(self, req, resp):
-        """Return top page."""
-        with open('app/index.html', 'r') as f:
-            resp.body = f.read()
-        resp.content_type = falcon.MEDIA_HTML
-        resp.status = falcon.HTTP_OK
+        """Redirect to '/index.html'."""
+        raise falcon.HTTPMovedPermanently('/index.html')
--- tests/test_root.py
+++ tests/test_root.py
@@ -6,4 +6,5 @@ import falcon
 def test_get_root(client):
     """Check if 'GET /' is redirected to 'GET /index.html'"""
     response = client.simulate_get('/')
+    assert response.headers['location'] == '/index.html'
     assert response.status == falcon.HTTP_MOVED_PERMANENTLY
