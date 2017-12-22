047ea243b6c1974368827290959fcaa184e60965
it_akumi <it.akumi1011@gmail.com>
2017-12-22 15:59:30 +0900

    Add basic flow of authorization to authorize

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,26 @@
 # coding:utf-8
+import falcon
+
+
 def authorize(req, resp, _):
-    pass
+    """Check if request has valid authorization token."""
+    token = req.get_header('Authorization')
+
+    if token is None:
+        description = 'Please provide an auth token.'
+        raise falcon.HTTPUnauthorized(
+            'Auth token required',
+            description
+        )
+
+    if not token_is_valid(token):
+        description = 'The provided auth token is not valid.'
+        raise falcon.HTTPUnauthorized(
+            'Authorization required',
+            description
+        )
+
+
+def token_is_valid(token):
+    """Check if authorization token is valid."""
+    return True
